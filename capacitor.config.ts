ed6befc57d475f0b2a65d5b68e0206e665e9280d
Abellot <abellot@nervia.es>
2024-06-06 13:45:50 +0200

    updated config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.agap2.arian',
   appName: 'w2m',
   webDir: 'www'
 };
