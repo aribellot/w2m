74e4e9d3444f10e57f5ed2f89912b3dea8695e53
Abellot <abellot@nervia.es>
2024-06-07 10:28:33 +0200

    updated assets

--- src/app/error/error.page.spec.ts
+++ src/app/error/error.page.spec.ts
@@ -1,26 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
-
+import { Router, provideRouter } from '@angular/router';
 import { ErrorPage } from './error.page';
 
 describe('ErrorPage', () => {
   let component: ErrorPage;
   let fixture: ComponentFixture<ErrorPage>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('Router', ['navigate']);
     await TestBed.configureTestingModule({
-      imports: [ErrorPage],
-      providers: [provideRouter([])]
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
+      imports: [ ErrorPage ],
+      providers: [
+        provideRouter([]),
+        { provide: Router, useValue: spy },
+      ],
+    })
+    .compileComponents();
     fixture = TestBed.createComponent(ErrorPage);
     component = fixture.componentInstance;
+    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to heroes list', () => {
+    component.navigateToList();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['heroes/list']);
   });
 });
--- src/app/heroes/heroes.page.spec.ts
+++ src/app/heroes/heroes.page.spec.ts
@@ -1,22 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
-
+import { Router, provideRouter } from '@angular/router';
+import { NavController } from '@ionic/angular';
 import { HeroesPage } from './heroes.page';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA, EnvironmentInjector } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { listOutline, createOutline } from 'ionicons/icons';
+import { ErrorPage } from '../error/error.page';
 
 describe('HeroesPage', () => {
   let component: HeroesPage;
   let fixture: ComponentFixture<HeroesPage>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeroesPage],
-      providers: [provideRouter([])]
-    }).compileComponents();
-  });
+    const spy = jasmine.createSpyObj('Router', ['navigate']);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroesPage);
+    await TestBed.configureTestingModule({
+      imports: [
+        HeroesPage,
+        IonTabs,
+        IonTabBar,
+        IonTabButton,
+        IonIcon,
+        IonLabel,
+      ],
+      providers: [
+        { provide: Router, useValue: spy },
+        EnvironmentInjector,
+        NavController,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    addIcons({ listOutline, createOutline });
+
+    fixture = TestBed.createComponent(ErrorPage);
     component = fixture.componentInstance;
+    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     fixture.detectChanges();
   });
 
